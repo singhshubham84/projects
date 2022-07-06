b86087e8d9533ec1270b2f81f03ede4cab0ed112
singhshubham84 <105306907+singhshubham84@users.noreply.github.com>
2022-07-06 12:44:43 +0530

    add2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,13 +21,13 @@ const isValidTitle = function (title) {
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 // regex for email validation
 const mobileRegex = /^[0]?[6789]\d{9}$/
- //10 didgit mobile number stating with any(6,7,8,9) and 0 if you want to use in mobile number 
+//10 didgit mobile number stating with any(6,7,8,9) and 0 if you want to use in mobile number 
 
 const passwordregex = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#@$%&? "])[a-zA-Z0-9!#@$%&?]{8,15}$/
 //  One digit, one upper case , one lower case , its b/w 8 to 15
- 
-const createUser = async function(req,res){
-    try{
+
+const createUser = async function (req, res) {
+    try {
         let = req.body
         if (!isValidRequestBody(data)) {
             return res.status(400).send({ status: false, msg: "Invalid request parameters. Please provide author details" })
@@ -35,18 +35,18 @@ const createUser = async function(req,res){
         //here checking the data come from request body should not be empty
 
         if (!isValid(title))
-        return res.status(400).send({ status: false, message: "Title is required" })
-    if (!isValidTitle(title)) {
-        return res.status(400).send({ status: false, message: "Title should be among Mr, Mrs, Miss" })
-    }
-    //validating the title with above two function
+            return res.status(400).send({ status: false, message: "Title is required" })
+        if (!isValidTitle(title)) {
+            return res.status(400).send({ status: false, message: "Title should be among Mr, Mrs, Miss" })
+        }
+        //validating the title with above two function
 
-        if(!isValid(name))
-        return res.status(400).send({status:false, message : "First name is required"})
+        if (!isValid(name))
+            return res.status(400).send({ status: false, message: "First name is required" })
         //    validating the name with regex       
- 
-        if(!isValid(phone))
-        return res.status(400).send({status:false, message : "phone number is required"})
+
+        if (!isValid(phone))
+            return res.status(400).send({ status: false, message: "phone number is required" })
         //    validating the name with regex       
         if (!(mobileRegex.test(phone))) {
             return res.status(400).send({ status: false, message: "Please provide a valid mobile number, it should start 6-9.(you can also use STD code 0)" })
@@ -61,28 +61,28 @@ const createUser = async function(req,res){
         }
         // validating the email with regex
 
-        let authorEmail = await authorModel.find({ email: data.email })
-        if (authorEmail.length !== 0)
+        let userEmail = await userModel.find({ email: data.email })
+        if (userEmail.length !== 0)
             return res.status(401).send({ status: false, message: "This e-mail address is already exist , Please enter valid E-mail address" })
         //checking the author email is correct or not 
 
-        if(!isValid(password))
-        return res.status(400).send({status:false, message:"password is not exist"})
-       // password is present or not
+        if (!isValid(password))
+            return res.status(400).send({ status: false, message: "password is not exist" })
+        // password is present or not
 
         if (!(passwordregex.test(password))) {
             return res.status(400).send({ status: false, message: "password should contain at least One digit, one upper case , one lower case , its b/w 8 to 15" })
         }
         //validating the email with regex
 
-        let authorCreated = await authorModel.create(data)
-        return res.status(201).send({ status: true, message: "Author created successfully", data: authorCreated })
+        let userCreated = await authorModel.create(data)
+        return res.status(201).send({ status: true, message: "Author created successfully", data: userCreated })
 
     }
-    catch(err){
-        return res.status(500).send({message:"Error",error:err.message})
+    catch (err) {
+        return res.status(500).send({ message: "Error", error: err.message })
     }
 
 }
 
-module.exports.createUser=createUser
+module.exports.createUser = createUser
