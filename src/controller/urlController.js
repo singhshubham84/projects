5cbc43c46d5284a217a22b39e318cc5bc4e7b98e
shivamd-96 <shivamd96@outlook.com>
2022-07-18 15:46:20 +0530

    create url update

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -10,12 +10,17 @@ const createUrl = async function (req, res) {
   try {
     const longUrl = req.body.longUrl
 
-    if (!validUrl.isUri(longUrl)) { return res.status(400).send({ status: false, message: "invalid URL" })} 
-
+    if (!validUrl.isUri(longUrl)) {
+      return res.status(400).send({ status: false, message: "invalid URL" })
+    }
+    const isExistUrl = await urlModel.findOne({longUrl});
+    if( isExistUrl) {
+      return res.status(201).send({status:true, message:"sucess", data: isExistUrl})
+    }
     const str = 'http://localhost:3000/'
     const urlCode = shortid.generate()
-    const shortUrl = str + urlCode 
-    const savedData = await urlModel.create({longUrl, shortUrl, urlCode})
+    const shortUrl = str + urlCode
+    const savedData = await urlModel.create({ longUrl, shortUrl, urlCode })
     return res.status(201).send({ status: true, message: "success", data: savedData })
 
   }
@@ -26,21 +31,21 @@ const createUrl = async function (req, res) {
 
 // ========> get url
 const getUrl = async function (req, res) {
-    try {
-        
-    const urlCode = req.params.urlCode
-    const getUrl  = await urlModel.findOne({ urlCode });
+  try {
 
-    if (!shortid.isValid(urlCode)) { return res.status(400).send({ status: false, message: "invalid URL" })} 
-    if (!getUrl) { return res.status(404).send({ status: false, message: "URL not found" })} 
+    const urlCode = req.params.urlCode
+    const getUrl = await urlModel.findOne({ urlCode });
+
+    if (!shortid.isValid(urlCode)) { return res.status(400).send({ status: false, message: "invalid URL" }) }
+    if (!getUrl) { return res.status(404).send({ status: false, message: "URL not found" }) }
 
     return res.status(302).send({ status: true, message: "success", data: getUrl.longUrl })
-  
-    }
-    catch (err) {
-      return res.status(500).send({ status: false, message: err.message })
-    }
+
   }
+  catch (err) {
+    return res.status(500).send({ status: false, message: err.message })
+  }
+}
 
 module.exports.createUrl = createUrl
 module.exports.getUrl = getUrl
