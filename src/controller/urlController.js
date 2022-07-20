e63f4e50c28a0f85e646480578bd54d6c5207dfe
singhshubham84 <105306907+singhshubham84@users.noreply.github.com>
2022-07-20 17:47:36 +0530

    cache

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -95,14 +95,14 @@ const getUrl = async function (req, res) {
         return res.status(302).redirect(cachedLongUrl)
     }
 
-    
-   
 
     if (!shortid.isValid(urlCode)) { return res.status(400).send({ status: false, message: "invalid URL" }) }
 
     const getUrl = await urlModel.findOne({ urlCode });
 
     if (!getUrl) { return res.status(404).send({ status: false, message: "URL not found" }) }
+
+    await SET_ASYNC(`${urlCode}`, JSON.stringify(getUrl))
 
     return res.status(302).redirect(getUrl.longUrl)
 
