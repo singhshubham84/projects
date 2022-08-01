ebbb798f3077f28f1e3a4fe096a3a98fb00fb9cf
singhshubham84 <105306907+singhshubham84@users.noreply.github.com>
2022-08-01 17:47:30 +0530

    add8

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -4,39 +4,41 @@ const {  isValidObjectId,isValidRequestBody } = require("../validator/validator"
 const  cartModel=require("../models/cartModel")
 
 
-const creatCart=async function (req,res){
+const createCart=async function (req,res){
     try{
-        const userId= req.param.userId
+        const userId= req.params.userId
         const data= req.body
         const {productId}= data
-        
-   // validation 
-      if(!!isValidRequestBody(data)){
-        return res.status(400).send({status:false, msg: "please provide request body" }) 
-      }
-      if(!isValidObjectId(userId)){
-        return res.status(400).send({status:false, msg: " please provide userId"})
-      }
-      if (!isValidObjectId(productId)) {
-        return res.status(400).send({ status: false, message: "Invalid ProductId" })
-    }
-     const findUser= await userModel.findById({_id:userId})
+        let userIdFromToken = req.userId
+
+        if (!isValidObjectId(userId)) {return res.status(400).send({ status: false, message: "Invalid userId in path params." })}
+       
+        if(!isValidRequestBody(data)){
+            return res.status(400).send({status:false, msg: "please provide request body" }) 
+          }
+        const findUser= await userModel.findById({_id:userId})
         if(!findUser){
              return res.status(400).send({status:false,msg:"userId not exist"})
         }
+
+        if (userId!= userIdFromToken) {return res.status(401).send({ status: false, message: `authentication fail ` })}
+        
+     
+      if (!isValidObjectId(productId)) {
+        return res.status(400).send({ status: false, message: "Invalid ProductId" })
+    }
+     
         const findProduct= await productModel.findOne({_id:productId,isDeleted:false})
         if(!findProduct){
             return res.status(400).send({status:false,msg:"productid doesnt exist"})
         }
 
-       
-        
         // finding cart related to user.
         const findCartOfUser = await cartModel.findOne({ userId: userId })
 
         if (!findCartOfUser) {
 
-            let priceSum = findProduct.price * 1
+            let priceSum = findProduct.price 
             let itemArr = [{ productId: productId, quantity: 1 }]
 
             const newUserCart = await cartModel.create({
@@ -51,7 +53,7 @@ const creatCart=async function (req,res){
         if (findCartOfUser) {
 
             //updating price when products get added or removed
-            let price = findCartOfUser.totalPrice + (1 * findProduct.price)
+            let price = findCartOfUser.totalPrice + ( findProduct.price)
             let arr = findCartOfUser.items
          // updating quentity
 
@@ -211,7 +213,7 @@ const deleteCart = async function (req, res) {
     }
 }
 
-module.exports.creatCart = creatCart;
+module.exports.createCart = createCart;
 module.exports.removeProductInCart=removeProductInCart;
 module.exports.addProductInCart=addProductInCart;
 module.exports.getCartData=getCartData;
--- src/route/route.js
+++ src/route/route.js
@@ -3,27 +3,27 @@ const router = express.Router();
 const userController = require('../controllers/userController')
 const productController = require('../controllers/productController')
 const auth = require('../middleware/auth')
+const cartController=require('../controllers/cartControllers')
 
 /*_____________________-----===> API FOR USER <====-----______________________________*/ 
 
 router.post("/register", userController.createUser)
-
 router.post("/login",userController.userLogin)
-
 router.get('/user/:userId/profile',auth.userAuthentication, userController.getUserDetails)
-
 router.put('/user/:userId/profile',auth.userAuthentication, userController.updateUserDetails)
 
 /**______________________----==> PRODUCT API <===---____________________________________ */
 
 router.post("/products", productController.createProduct)
-
 router.get('/products',productController.getAllProducts)
-
 router.get('/products/:productId',productController.getProductById)
-
 router.put('/products/:productId',productController.updateProduct)
-
 router.delete('/products/:productId',productController.deleteProduct)
+
+/**_____________________________________________________________________________________ */
+router.post('/users/:userId/cart',auth.userAuthentication, cartController.createCart)
+
+
+
 
 module.exports = router;
